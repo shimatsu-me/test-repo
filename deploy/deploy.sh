1d51a05b02223935720333d4eebd3ade49c6fe42
shimatsu-me <shimatsu-me@ibjapan.jp>
2024-07-10 10:55:25 +0900

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 echo '<<<<<<ここです！<<<<<<<<'
-echo "TABLE_CHECK_WEB_BOOKING_URL=${TABLE_CHECK_WEB_BOOKING_URL}" > .env
-cat .env
+echo ${TABLE_CHECK_WEB_BOOKING_URL}
 echo '<<<<<<ここです！<<<<<<<<'
