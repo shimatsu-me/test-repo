3898238ddae9d63f8c446b14ba3a98c990c758ed
shimatsu-me <shimatsu-me@ibjapan.jp>
2024-07-10 10:45:59 +0900

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 echo '<<<<<<ここです！<<<<<<<<'
-echo "$(echo ${TABLE_CHECK_WEB_BOOKING_URL} | base64 --decode)"
+
+# 環境変数の値を直接出力する場合
+echo "${TABLE_CHECK_WEB_BOOKING_URL}"
+
+# もし環境変数が base64 エンコードされている場合、デコードして出力する例
+if [[ "${TABLE_CHECK_WEB_BOOKING_URL}" == *"="* ]]; then
+  decoded_url=$(echo "${TABLE_CHECK_WEB_BOOKING_URL}" | base64 --decode)
+  echo "${decoded_url}"
+fi
+
 echo '<<<<<<ここです！<<<<<<<<'
