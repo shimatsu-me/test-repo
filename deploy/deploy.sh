fda6eb4759603eb66b14b6c220ada6e819c65f60
shimatsu-me <shimatsu-me@ibjapan.jp>
2024-07-10 10:54:24 +0900

    test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo '<<<<<<ここです！<<<<<<<<'
-echo ${TABLE_CHECK_WEB_BOOKING_URL} > .env
-echo .env
+echo "TABLE_CHECK_WEB_BOOKING_URL=${TABLE_CHECK_WEB_BOOKING_URL}" > .env
+cat .env
 echo '<<<<<<ここです！<<<<<<<<'
